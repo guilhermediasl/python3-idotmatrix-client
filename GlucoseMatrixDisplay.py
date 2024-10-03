b104f7f957f8267a016b44fabc61efeba20609fb
Guilherme Dias de Andrade Lima <guilherem.de.lima@gmail.com>
2024-10-03 10:24:22 -0300

    Update GlucoseMatrixDisplay.py

--- GlucoseMatrixDisplay.py
+++ GlucoseMatrixDisplay.py
@@ -501,7 +501,7 @@ class Color:
     purple = [250, 0, 105]
     white = [230, 170, 80]
     blue = [20, 150, 135]
-    orange = [190, 90, 20]
+    orange = [255, 170, 0]
 
 class GlucoseItem:
     def __init__(self, type, glucose, dateString, direction = None):
