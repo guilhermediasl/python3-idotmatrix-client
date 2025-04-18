9aee5d55ec81fa2fdfc10f1869eda67b08bf43cb
Guilherme Dias de Andrade Lima <guilherem.de.lima@gmail.com>
2025-04-18 15:15:27 -0300

    Change value calculation to use rounding instead of ceiling for improved color accuracy in fade_color method

--- GlucoseMatrixDisplay.py
+++ GlucoseMatrixDisplay.py
@@ -355,7 +355,7 @@ class GlucoseMatrixDisplay:
             elif i == 2:  # Blue
                 LOW_BRIGHTNESS_CORRECTION = 5
                 correction = 1 - (1 - Color.BLUE_CORRECTION) * (1 - percentil) ** LOW_BRIGHTNESS_CORRECTION
-            value = math.ceil(item * percentil * correction)
+            value = round(item * percentil * correction)
             corrected_color.append(min(255, value))
         return corrected_color
 
